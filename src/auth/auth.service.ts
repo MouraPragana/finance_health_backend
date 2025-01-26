ec8b522c417a3240d5c2a0817a6dc8ae2e37f5a8
Mateus de Moura Pragana <mateuspragana@gmail.com>
2025-01-26 16:22:23 -0300

    fix: space on code

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,28 +6,28 @@ import { AuthDto } from './dto/auth.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(
-    private userService: UsersService,
-    private jwtService: JwtService,
-  ) {}
+    constructor(
+        private userService: UsersService,
+        private jwtService: JwtService,
+    ) {}
 
-  async login(authDto: AuthDto) {
-    const user = await this.userService.findOneByEmail(authDto.email);
-    if (!user) {
-      throw new BadRequestException(
-        'Não foi possível localizar o e-mail em questão.',
-      );
+    async login(authDto: AuthDto) {
+        const user = await this.userService.findOneByEmail(authDto.email);
+        if (!user) {
+            throw new BadRequestException(
+                'Não foi possível localizar o e-mail em questão.',
+            );
+        }
+
+        const match = await bcrypt.compare(authDto.password, user.password);
+        if (!match) {
+            throw new BadRequestException('E-mail ou Senha inválidos.');
+        }
+
+        const payload = { sub: user.id, uusername: user.email };
+
+        return {
+            access_token: await this.jwtService.signAsync(payload),
+        };
     }
-
-    const match = await bcrypt.compare(authDto.password, user.password);
-    if (!match) {
-      throw new BadRequestException('E-mail ou Senha inválidos.');
-    }
-
-    const payload = { sub: user.id, uusername: user.email };
-
-    return {
-      access_token: await this.jwtService.signAsync(payload),
-    };
-  }
 }
