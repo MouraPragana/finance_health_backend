c8ff6063319c287209727b6b3fcf26050b5d9ecf
Mateus de Moura Pragana <mateuspragana@gmail.com>
2025-01-26 15:34:19 -0300

    feat: creating auth module

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UsersService } from 'src/users/users.service';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+
+@Module({
+  controllers: [AuthController],
+  providers: [AuthService, UsersService, PrismaService],
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: process.env.SECRET,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+})
+export class AuthModule {}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,33 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { UsersService } from 'src/users/users.service';
+import { AuthDto } from './dto/auth.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async login(authDto: AuthDto) {
+    const user = await this.userService.findOneByEmail(authDto.email);
+    if (!user) {
+      throw new BadRequestException(
+        'Não foi possível localizar o e-mail em questão.',
+      );
+    }
+
+    const match = await bcrypt.compare(authDto.password, user.password);
+    if (!match) {
+      throw new BadRequestException('E-mail ou Senha inválidos.');
+    }
+
+    const payload = { sub: user.id, uusername: user.email };
+
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
+  }
+}
