b7e4c56dbf5c0d040dc80104c12fc7deae37f8c2
Mateus de Moura Pragana <mateuspragana@gmail.com>
2025-01-26 16:17:21 -0300

    feat: adjusting code

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,38 +5,40 @@ import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
-  constructor(private prisma: PrismaService) {}
+    constructor(private prisma: PrismaService) {}
 
-  async create(createUserDto: CreateUserDto) {
-    const hasAlreadyRegistered = await this.findOneByEmail(createUserDto.email);
+    async create(createUserDto: CreateUserDto) {
+        const hasAlreadyRegistered = await this.findOneByEmail(
+            createUserDto.email,
+        );
 
-    if (hasAlreadyRegistered) {
-      throw new BadRequestException(
-        'E-mail já cadastrado em nossa plataforma.',
-      );
+        if (hasAlreadyRegistered) {
+            throw new BadRequestException(
+                'E-mail já cadastrado em nossa plataforma.',
+            );
+        }
+
+        const saltOrRounds = 10;
+        const hash = await bcrypt.hash(createUserDto.password, saltOrRounds);
+
+        const data = {
+            email: createUserDto.email,
+            password: hash,
+        } as CreateUserDto;
+
+        return this.prisma.user.create({
+            data,
+            omit: {
+                password: true,
+            },
+        });
     }
 
-    const saltOrRounds = 10;
-    const hash = await bcrypt.hash(createUserDto.password, saltOrRounds);
-
-    const data = {
-      email: createUserDto.email,
-      password: hash,
-    } as CreateUserDto;
-
-    return this.prisma.user.create({
-      data,
-      omit: {
-        password: true,
-      },
-    });
-  }
-
-  async findOneByEmail(email: string) {
-    return await this.prisma.user.findFirst({
-      where: {
-        email,
-      },
-    });
-  }
+    async findOneByEmail(email: string) {
+        return await this.prisma.user.findFirst({
+            where: {
+                email,
+            },
+        });
+    }
 }
